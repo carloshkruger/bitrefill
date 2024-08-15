9c6e676a13f77bf7fc8f5bea33b63242192325e8
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-08-15 16:45:44 -0300

    exporting more resources in the index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export { Bitrefill, BitrefillProps } from "./bitrefill";
+export { BitrefillError } from "./utils/bitrefill-error";
+export * from "./common/types";
+export * from "./resources/invoices/types";
+export * from "./resources/misc/types";
+export * from "./resources/orders/types";
+export * from "./resources/products/types";
